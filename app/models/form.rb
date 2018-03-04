bed345a5e1e3011eee4e25e5fc526db589fddd60
Caelan L. Talmadge <talmadge12@gmail.com>
2018-03-03 21:46:16 -0600

    Form controller populated with methods. Form model populated with pertinent database commands.

--- app/models/form.rb
+++ app/models/form.rb
@@ -1,2 +1,16 @@
 class Form < ApplicationRecord
+
+attr_accessible :name, :price, :form_type_id, :properties
+belongs_to :form_type
+serialize :properties, Hash
+
+validate :validate_properties
+
+  def validate_properties
+    form_type.fields.each do |field|
+      if field.required? && properties[field.name].blank?
+        errors.add field.name, "must not be blank"
+      end
+    end
+  end
 end

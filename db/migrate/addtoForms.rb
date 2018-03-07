8f135f4abbe55c60a9e2fe0965fa1772e6f96f2f
Hao Zhou <hzhou3@go.olemiss.edu>
2018-03-06 23:47:30 -0600

    Delete addtoForms.rb

--- db/migrate/addtoForms.rb
+++
@@ -1,8 +0,0 @@
-class AddattrsToForms < ActiveRecord::Migration[5.1]
-  def change
-    add_column :forms, :form_status, :bigint
-    add_column :forms, :com_id, :bigint
-    add_column :forms, :user_webID, :string
-
-  end
-end

8c8a66f8ce168bb7d83bde2bd8417f31215d4a2e
Caelan L. Talmadge <talmadge12@gmail.com>
2018-04-02 22:14:30 -0500

    Fix badd migration.

--- db/migrate/20180304052920_add_name_to_form_fields.rb
+++ db/migrate/20180304052920_add_name_to_form_fields.rb
@@ -2,5 +2,4 @@ class AddNameToFormFields < ActiveRecord::Migration[5.1]
   def change
     add_column :form_fields, :name, :string
   end
-  add_index "form_fields", ["form_type_id"], :name => "index_form_fields_on_form_type_id"
 end

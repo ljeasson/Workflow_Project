75e844aed453bc255d9d6e1fa4c332db3e4afc10
Caelan L. Talmadge <talmadge12@gmail.com>
2018-04-03 17:19:54 -0500

    Added assignments model to database. Updated user model to support a has_many assignments, has_many roles through assignments

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,10 @@
 class User < ActiveRecord::Base
 
+has_secure_password
+
+has_many :assignments
+has_many :roles, through: :assignments
+
   def self.from_omniauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_initialize.tap do |user|
       user.provider = auth.provider

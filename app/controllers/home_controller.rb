7314f006299dc2ef32c0db69604da0e99e21a540
Caelan L. Talmadge <talmadge12@gmail.com>
2018-04-05 01:01:05 -0500

    Roles and admin assignments complete.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,5 +4,17 @@ class HomeController < ApplicationController
     if !(Role.where(:name => 'admin').present?)
       Role.create(:name => 'admin')
     end
+
+
+    #If the user's ID is present in the system(whatever it might be), check if
+    # an assignment hasn't already been created. If not, assign("hardcode") the user
+    # to the admin role.
+    if (User.where(id: User.where(email: "cltalmad@go.olemiss.edu")).present?)
+      user = User.where(:email => "cltalmad@go.olemiss.edu").first
+      admin = Role.where(:name => 'admin').first
+      if !(Assignment.find_by(id: user.id).present?)
+        Assignment.create(:user_id => user.id, :role_id => admin.id)
+      end
+    end
   end
 end
